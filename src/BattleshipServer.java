3cdfdb4f1da5f8cf292080a3bed4297f9d4b6db7
pjr8848 <56843283+pjr8848@users.noreply.github.com>
2019-10-31 14:28:06 -0400

    Skeleton code
    Broke the notes up from 10/31 and put them into their own 'Outer Classes' with some Psuedo Code

---
+++ src/BattleshipServer.java
@@ -0,0 +1,46 @@
+//SERVER
+
+//SERVER RECIEVES ARRAY OF SHIPS
+
+//SERVER BUILDS THE GRID
+//play game method, once both players are built sleep (short period of time)
+//game starts (do-while, while(win-condition: false) -> make it a thread?
+//take turn methods? (2 ints that are pushed into the method -> shots coordinates)
+//player1 take turn? player2 take turn? 
+//on win-condition we break out, send health pools back into the client.
+//Currently NO TURNS
+
+//WHEREVER THERE IS A SHIP IT WILL PLACE A TRUE, EVERYWHERE ELSE WILL BE FALSE
+
+//server waits until it gets both array, builds them in the order they come in
+
+//wait for turns, after they hit the ready (Message back from server confirmation that the game is starting/grid was valid)
+
+
+
+public class BattleshipServer
+{//open class
+   Boolean Win_Condition = false;
+   Grid player1;
+   Grid player2;
+   Boolean[][] PlayField = new Boolean[10][10];
+   //may need to take in Player1 and Player2??
+   BattleshipServer(/* Maybe take an Array of Coordinates for the ships? or an Array of Ships if ships implemented properly*/)
+   {//open constructor
+   
+      //This should build a new version of the Grid from Grid, for each player!
+      //This will deal with hit-handling, saving grids, HP, running the game
+      //method for running game (Do-While)
+      
+      
+   }//close constructor
+   
+   //If we don't thread it we can just use Player1.getHP() / Player2.getHP(), whichever player's current turn is going
+   // if we thread it, we should be able to do Player.getHP() regardless.
+   //Method? or just Check at start of each turn?
+   //if Player.getHP() == 0
+   //    Win_Condition = true;
+   //else
+   //    
+   
+}//close class
---
+++ src/Ships.java
@@ -0,0 +1,20 @@
+//ALL SHIPS ARE INSTANCE OF THIS CLASS!
+//take all attributes used to create the ship add it to a global array once their all in.
+
+//USE GETTERS AND SETTERS FOR EVERYTHING
+
+//once client recieves that change the GUI to how we actually play the game.
+
+
+public class Ships
+{//open class
+   Ships(String Name, int Arraylength, int X, int Y, boolean Orientation)
+   {//open constructor
+   
+      //Array, two ints, orientation
+      
+      //I'M NOT SURE HOW THIS IS GOING TO BE IMPLEMENTED, BUT I'M NEED TO SEE IT BEFORE I CONTINUE PSUEDO CODE ON THE OTHER CLASSES
+      //I believe 4 Outter Classes will be more than enough to handle the game (and as many inner/anonymous classes as we need)
+   
+   }//close constructor
+}//close class
