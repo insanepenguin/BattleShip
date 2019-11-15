38cc360f7d8709b06a64d9ef6ce292084f95564c
bbs1219 <56843307+bbs1219@users.noreply.github.com>
2019-11-15 16:45:47 -0500

    made progress on chatting in server

--- src/BattleshipServer.java
+++ src/BattleshipServer.java
@@ -37,8 +37,6 @@ public class BattleshipServer
    
    ServerSocket ss;
    Socket cs;
-   ObjectInputStream ois;                  //declare globally!
-   ObjectOutputStream oos;
    Ship[] shipsReadIn;
    
    private JFrame jfServerFrame = new JFrame();
@@ -76,6 +74,7 @@ public class BattleshipServer
       jfServerFrame.pack();
       jfServerFrame.setVisible(true);
       Thread[] playersReady = new Thread[2];
+      Socket[] sockets = new Socket[2];
       try
       {//open try
          jtaDiagnostics.setText("Server launched @" + InetAddress.getLocalHost().getHostAddress());
@@ -83,8 +82,8 @@ public class BattleshipServer
          for(int i = 0; i < 2; i++)
          {//open for loop
             cs = ss.accept();
-            ShipReciever thread = new ShipReciever(cs, i);
-            Connection chatHander = new Connection(cs, i);
+            sockets[i] = cs;
+            ShipReceiver thread = new ShipReceiver(cs, i);
             playersReady[i] = thread;
             thread.start();
             console("Player " + (i + 1) + " Connected @" + cs.getInetAddress());       //update text area saying someone connected
@@ -100,12 +99,13 @@ public class BattleshipServer
       for(int i = 0; i < 2; i++) {
          try {
             playersReady[i].join();
-            System.out.println("WE HAVE JOINED"); // test statement
+            new Connection(sockets[i], i).start();
          }
          catch(InterruptedException ie) {
             ie.printStackTrace();
          }
       }
+
       console("Ships recieved, starting game");
       System.out.println("Ships recieved, starting game");
       
@@ -114,41 +114,42 @@ public class BattleshipServer
       THIS IS WHERE THE LOGIC OF THE GAME IS ACTUALLY HAPPENING
       PAY ATTENTION HERE!
       THIS IS ALL THE NEW STUFF (plus hitDetection Method)*/
-      try{//open try  
-         ois = new ObjectInputStream(cs.getInputStream());
-         oos = new ObjectOutputStream(cs.getOutputStream());
-            oos.writeBoolean(win_Condition);
-            System.out.println("writing Win Condition");
-         while(!win_Condition){//open while loop
-            oos.writeBoolean(yourTurn);
-            System.out.println("Test");
-            String inCoords = ois.readUTF();
-            String[] Coords = inCoords.split(", ");
-            int xCoord = Integer.parseInt(Coords[0]);
-            int yCoord = Integer.parseInt(Coords[1]);
-            console("Coordinates to shoot are " + xCoord + " and " + yCoord);
-            hitDetection(xCoord, yCoord);
-            oos.writeBoolean(win_Condition);
-            oos.flush();
-         }//close while loop
-         oos.writeUTF(winner);
-      }//close try
-      catch(IOException ioe){//open catch
-         ioe.printStackTrace();
-      }//close catch
-      
-      int startingHP = 0;
-      for(Boolean[] x : player1Field){
-         for (boolean y : x){
-            if(y){
-               startingHP++;
-            }
-         }
-      }
-      //TODO:Add player instantiations and start the game
-      player1 = new Player(player1Field,startingHP);
-      player2 = new Player(player2Field,startingHP);
-      Game(player1,player2);
+      // try{//open try  
+//          ObjectInputStream turnReader = new ObjectInputStream(cs.getInputStream());
+//          // ObjectOutputStream turnWriter = new ObjectOutputStream(cs.getOutputStream());
+//          // turnWriter.writeBoolean(win_Condition);
+//          // System.out.println("writing Win Condition");
+//          while(!win_Condition){//open while loop
+//             // turnWriter.writeBoolean(yourTurn);
+//             System.out.println("Test");
+//             String inCoords = turnReader.readUTF();
+//             String[] Coords = inCoords.split(", ");
+//             int xCoord = Integer.parseInt(Coords[0]);
+//             int yCoord = Integer.parseInt(Coords[1]);
+//             console("Coordinates to shoot are " + xCoord + " and " + yCoord);
+//             hitDetection(xCoord, yCoord);
+//             // turnWriter.writeBoolean(win_Condition);
+//             // turnWriter.flush();
+//          }//close while loop
+//          // turnWriter.writeUTF(winner);
+//          // turnWriter.flush()
+//       }//close try
+//       catch(IOException ioe){//open catch
+//          ioe.printStackTrace();
+//       }//close catch
+//       
+//       int startingHP = 0;
+//       for(Boolean[] x : player1Field){
+//          for (boolean y : x){
+//             if(y){
+//                startingHP++;
+//             }
+//          }
+//       }
+//       //TODO:Add player instantiations and start the game
+//       player1 = new Player(player1Field,startingHP);
+//       player2 = new Player(player2Field,startingHP);
+//       Game(player1,player2);
       
    }//close constructor
    
@@ -158,21 +159,25 @@ public class BattleshipServer
       jtaDiagnostics.setText(jtaDiagnostics.getText() + '\n' + msg);
    }//close console updating method
    
-   public class ShipReciever extends Thread {
+   public class ShipReceiver extends Thread {
       Socket sock;
       int player;
       
-      public ShipReciever(Socket cs, int i) {
+      public ShipReceiver(Socket cs, int i) {
          cs = sock;
          player = i;
+      }
+      
+      public Socket getSock() {
+         return sock;
       }
       
       public void run() {
          try
          {//open try
-            ois = new ObjectInputStream(cs.getInputStream());                       //instantiate locally!
+            ObjectInputStream shipReader = new ObjectInputStream(cs.getInputStream());                       //instantiate locally!
             System.out.println("we should be reading in a new object of ship arrays");
-            Object incoming = ois.readObject();
+            Object incoming = shipReader.readObject();
             System.out.println(incoming);
             if(incoming instanceof Ship[])                     //compare if what came in is an instance of an array of the Ship class
             {//open if                                         if it is, make our Ship array equal to the incoming object casted as a ship array
@@ -214,7 +219,7 @@ public class BattleshipServer
             {//open else                                         if it isn't, print an error message to the text area
                console("ERROR! OBJECT READ IN WASN'T A SHIP!");
             }//close else
-            ois.close();
+            // shipReader.close();
          }//close try
          catch(IOException ioe)
          {//open catch
@@ -244,12 +249,14 @@ public class BattleshipServer
          new Reciever().start();
          try {
             PrintWriter chatWriter = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
+            console(getName() + "initialized");
             while(sock.isConnected()) {
                synchronized(sync) {
                   sync.wait(); //when the reciever recieves something, it updates the global string currentmsg then notifies
                }
                chatWriter.println(currentMsg); //once notified, the sender sends out msg to the clients
                chatWriter.flush();
+               console("message send out");
             }
          }
          catch(InterruptedException ie) {
@@ -272,10 +279,16 @@ public class BattleshipServer
          public void run() {
             try {
                BufferedReader chatReader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+               console(getName() + "initialized");
+               synchronized(sync) {
+                  currentMsg = "Player connected @" + sock.getInetAddress();
+                  sync.notifyAll();
+               }
                while(sock.isConnected()) {
                   String incoming = chatReader.readLine();
                   synchronized(sync) {
                      currentMsg = "[Player " + playerNum + "]: " + incoming;
+                     console(currentMsg);
                      sync.notifyAll();
                   }
                }
@@ -288,28 +301,28 @@ public class BattleshipServer
       }
    }
    
-   public void Game(Player p1,Player p2) {
-   //Wait for both players to pass in cords and then play the turn
-
-      while(!win_Condition){
-         //Wait for the cords to be sent
-         //Needs 4 ints 2xs and 2ys one pair form each player.
-         synchronized (sync){
-            try {
-               sync.wait();
-            } 
-            catch (InterruptedException e) {
-               e.printStackTrace();
-            }
-         }
-         int p1T= p1.turn(0,0);//Retruns a number to help send msg to ppl
-         int p2T= p2.turn(0,0);
-         System.out.println(p1T+"||"+p2T);
-         if( player1.isWinner() || player2.isWinner()){
-            win_Condition = true;
-         }//close if
-      }//close while
-   }//close game method
+   // public void Game(Player p1,Player p2) {
+//    //Wait for both players to pass in cords and then play the turn
+// 
+//       while(!win_Condition){
+//          //Wait for the cords to be sent
+//          //Needs 4 ints 2xs and 2ys one pair form each player.
+//          synchronized (sync){
+//             try {
+//                sync.wait();
+//             } 
+//             catch (InterruptedException e) {
+//                e.printStackTrace();
+//             }
+//          }
+//          int p1T= p1.turn(0,0);//Retruns a number to help send msg to ppl
+//          int p2T= p2.turn(0,0);
+//          // System.out.println(p1T+"||"+p2T);
+//          if( player1.isWinner() || player2.isWinner()){
+//             win_Condition = true;
+//          }//close if
+//       }//close while
+//    }//close game method
 
    //This is the code that will handle if something is hit
    public void hitDetection(int x, int y){//open hitDetection method
